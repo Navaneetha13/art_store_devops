46b3e1c0356763017d5b99a89980657cb6ce4bc5
Navaneetha13 <navaneethathalakokkula@gmail.com>
2025-12-02 10:22:16 +0000

    django tests

--- artapp/tests.py
+++ artapp/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.test import TestCase, Client
+from django.urls import reverse
+from art_store import views   
+
+class URLTests(TestCase):
+
+    def test_index_page_loads(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+        
+        
+    def test_view_artproduct_invalid_id(self):
+        response = self.client.get('/ViewArtProduct/9999')
+        self.assertEqual(response.status_code, 302)
+
+        
+    
+    def test_login_without_credentials(self):
+        response = self.client.post(reverse('doLogin'), {})
+        self.assertIn(response.status_code, [200, 302])
+
