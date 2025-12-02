cca7cb15ebdf883bf8cef6ad8d8584ae333fa365
Navaneetha13 <navaneethathalakokkula@gmail.com>
2025-12-02 10:12:51 +0000

    pylint test

--- art_store/settings.py
+++ art_store/settings.py
@@ -10,13 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
-
-
+import os
+from pathlib import Path
 import pymysql
 pymysql.install_as_MySQLdb()
 
-from pathlib import Path
-import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -86,24 +85,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'art_store.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'art-exhibition-management-rds',
-#         'USER': 'admin',
-#         'PASSWORD': 'art-exhibition-management-rds',
-#         'HOST': 'art-exhibition-management-rds.cxqospjboyrv.eu-west-2.rds.amazonaws.com',
-#         'PORT': '3306',
-        
-#     }
-# }
-
-
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -111,8 +92,6 @@ DATABASES = {
     }
 }
 
-# Password validation
-# https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -141,10 +120,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
@@ -154,9 +129,6 @@ STATIC_ROOT = 'staticfiles/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
 ]
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'artapp.CustomUser'
