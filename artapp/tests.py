d17048d17d5438d84ff46aaa690ec875038227bc
Navaneetha13 <navaneethathalakokkula@gmail.com>
2025-12-03 06:50:08 +0000

    SONAR

--- artapp/tests.py
+++ artapp/tests.py
@@ -14,10 +14,11 @@ class URLTests(TestCase):
     def test_view_artproduct_invalid_id(self):
         response = self.client.get('/ViewArtProduct/9999')
         self.assertEqual(response.status_code, 302)
-
         
     
     def test_login_without_credentials(self):
         response = self.client.post(reverse('doLogin'), {})
         self.assertIn(response.status_code, [200, 302])
+
+
 
