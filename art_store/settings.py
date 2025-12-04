d49c1dc17bd5807b40ad1ce0cfbeae1a724d6d54
Navaneetha13 <navaneethathalakokkula@gmail.com>
2025-12-04 06:36:48 +0000

    SONAR

--- art_store/settings.py
+++ art_store/settings.py
@@ -16,15 +16,10 @@ import pymysql
 pymysql.install_as_MySQLdb()
 
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-ls-#kv8apy^0%r@s2zq&m@e#(rg(!y-zz@ii_wvs&mf96-yu2l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
