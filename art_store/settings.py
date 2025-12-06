5a922d8764e9d454069e800b8ad69cd68bc2e8a1
Navaneetha13 <navaneethathalakokkula@gmail.com>
2025-12-06 12:53:54 +0000

    changes

--- art_store/settings.py
+++ art_store/settings.py
@@ -14,20 +14,17 @@ import os
 from pathlib import Path
 import pymysql
 pymysql.install_as_MySQLdb()
-
+from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+SECRET_KEY = get_random_secret_key()
 
-
-SECRET_KEY = 'django-insecure-ls-#kv8apy^0%r@s2zq&m@e#(rg(!y-zz@ii_wvs&mf96-yu2l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-
-
 
 # Application definition
 
